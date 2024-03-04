bdd959f96586a7cad17ac44f2d9095d8a9d6e7f0
Prayas <prayaspal04@gmail.com>
2024-03-05 01:15:24 +0530

    app

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from helpers import lookup
 
 app = Flask(__name__)
+CORS(app, origins="http://127.0.0.1:5173")
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/data')
+def get_data():
+    data = {'message': "hello, Flask here"}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
